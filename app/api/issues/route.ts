0ebe6132e8f263e7eeeedcb0c390c39839a9ea53
qasirm <harveymo@usc.edu>
2023-10-08 16:11:25 -0700

    set up radix

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -10,11 +10,12 @@ const createIssueSchema = z.object({
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
-    if (!validation.success)
+    if (!validation.success){
         return NextResponse.json(validation.error.errors, { status: 400 })
-    
-    const newIssue = prisma.issue.create({
+    }
+    const newIssue = await prisma.issue.create({
         data: {title: body.title, description: body.description}
-    })
-    return NextResponse.json(newIssue, { status: 201 })
+    });
+
+    return NextResponse.json(newIssue);
 }
