7b56cf97d2924087d47f61fe3b13f5e75e3b951c
qasirm <harveymo@usc.edu>
2023-10-11 17:50:06 -0700

    add loading skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Table } from "@radix-ui/themes";
-import IssueStatusBadge from "../components/IssueStatusBadge";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import IssuesToolbar from "./IssuesToolbar";
