6fd47632a48353345e322e3ef5b6d8df995ed4c5
qasirm <harveymo@usc.edu>
2023-10-12 19:54:55 -0700

    adding alert dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,20 +1,34 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   return (
-    <Button
-      color="red"
-      onClick={async () => {
-        await axios.delete("api/issue/" + issueId);
-        router.push("/issues");
-        router.refresh();
-      }}
-    >
-      Delete Issue
-    </Button>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button
+            color="red"
+            onClick={async () => {
+              await axios.delete("/api/issues/" + issueId);
+              router.push("/issues");
+              router.refresh();
+            }}
+          >
+            Delete Issue
+          </Button>
+          <AlertDialog.Content>
+            <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+            <AlertDialog.Description>
+              Are you sure you want to delete this issue? This action cannot be
+              undone.
+            </AlertDialog.Description>
+          </AlertDialog.Content>
+        </AlertDialog.Trigger>
+      </AlertDialog.Root>
+    </>
   );
 };
 
